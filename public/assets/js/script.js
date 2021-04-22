3ad48354bc68bca2cbe683fc28e54ae12e06492d
ATX834 <clementbacquet@hotmail.fr>
2021-04-22 16:56:06 +0200

    Correction script.js CLEAN PR

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,158 +1,193 @@
-//--------------- DEBUT JS MEDIAPLAYER ------------------ //
+//Initialisation API + définition du player
+// Avec musique
 
-function initialize() {
+var tag = document.createElement('script');
 
-    // Update the controls on load
-    updateTimerDisplay();
-    updateProgressBar();
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-    // Clear any old interval.
-    clearInterval(time_update_interval);
+// Récupère l'id du lien youtube
+// var. pas encore au point car on passe par l'id d'une div pour récupérer 
+// l'id du lien YT. A voir quand il y aura connexion vers BDD
 
-    // Start interval to update elapsed time display and
-    // the elapsed part of the progress bar every second.
-    time_update_interval = setInterval(function () {
+var lienVersYT = document.querySelector('#videoid').dataset.id;
+
+//Player avec les fonctionnalités
+
+var player,
+    time_update_interval = 0;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('video-placeholder', {
+        width: 600,
+        height: 400,
+        videoId: lienVersYT,
+        playerVars: {
+        },
+        events: {
+            onReady: initialize,
+            onStateChange: function (event) {
+                //permet de récupérer l'état du player
+            }
+        }
+    });
+
+    //--------------- DEBUT JS MEDIAPLAYER ------------------ //
+
+    function initialize() {
+
+        // Update the controls on load
         updateTimerDisplay();
         updateProgressBar();
-    }, 1000);
+
+        // Clear any old interval.
+        clearInterval(time_update_interval);
+
+        // Start interval to update elapsed time display and
+        // the elapsed part of the progress bar every second.
+        time_update_interval = setInterval(function () {
+            updateTimerDisplay();
+            updateProgressBar();
+        }, 1000);
 
 
-    $('#volume-input').val(Math.round(player.getVolume()));
-}
-
-
-// This function is called by initialize()
-function updateTimerDisplay() {
-    // Update current time text display.
-    $('#current-time').text(formatTime(player.getCurrentTime()));
-    $('#duration').text(formatTime(player.getDuration()));
-}
-
-
-// This function is called by initialize()
-function updateProgressBar() {
-    // Update the value of our progress bar accordingly.
-    $('#progress-bar').val((player.getCurrentTime() / player.getDuration()) * 100);
-}
-
-
-// Progress bar
-
-$('#progress-bar').on('mouseup touchend', function (e) {
-
-    // Calculate the new time for the video.
-    // new time in seconds = total duration in seconds * ( value of range input / 100 )
-    var newTime = player.getDuration() * (e.target.value / 100);
-
-    // Skip video to new time.
-    player.seekTo(newTime);
-
-});
-
-
-// Playback
-
-$('#play').on('click', function () {
-    player.playVideo();
-});
-
-
-$('#pause').on('click', function () {
-    player.pauseVideo();
-});
-
-
-// Sound volume
-
-
-$('#mute-toggle').on('click', function () {
-    var mute_toggle = $(this);
-
-    if (player.isMuted()) {
-        player.unMute();
-        mute_toggle.text('volume_up');
+        $('#volume-input').val(Math.round(player.getVolume()));
     }
-    else {
-        player.mute();
-        mute_toggle.text('volume_off');
+
+
+    // This function is called by initialize()
+    function updateTimerDisplay() {
+        // Update current time text display.
+        $('#current-time').text(formatTime(player.getCurrentTime()));
+        $('#duration').text(formatTime(player.getDuration()));
     }
-});
-
-$('#volume-input').on('change', function () {
-    player.setVolume($(this).val());
-});
 
 
-// Other options
+    // This function is called by initialize()
+    function updateProgressBar() {
+        // Update the value of our progress bar accordingly.
+        $('#progress-bar').val((player.getCurrentTime() / player.getDuration()) * 100);
+    }
 
 
-$('#speed').on('change', function () {
-    player.setPlaybackRate($(this).val());
-});
+    // Progress bar
 
-$('#quality').on('change', function () {
-    player.setPlaybackQuality($(this).val());
-});
+    $('#progress-bar').on('mouseup touchend', function (e) {
 
+        // Calculate the new time for the video.
+        // new time in seconds = total duration in seconds * ( value of range input / 100 )
+        var newTime = player.getDuration() * (e.target.value / 100);
 
-// Playlist
+        // Skip video to new time.
+        player.seekTo(newTime);
 
-$('#next').on('click', function () {
-    player.nextVideo()
-});
-
-$('#prev').on('click', function () {
-    player.previousVideo()
-});
+    });
 
 
-// Load video
+    // Playback
 
-$('.thumbnail').on('click', function () {
-
-    var url = $(this).attr('data-video-id');
-
-    player.cueVideoById(url);
-
-});
+    $('#play').on('click', function () {
+        player.playVideo();
+    });
 
 
-// Helper Functions
-
-function formatTime(time) {
-    time = Math.round(time);
-
-    var minutes = Math.floor(time / 60),
-        seconds = time - minutes * 60;
-
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-
-    return minutes + ":" + seconds;
-}
+    $('#pause').on('click', function () {
+        player.pauseVideo();
+    });
 
 
-$('pre code').each(function (i, block) {
-    hljs.highlightBlock(block);
-});
+    // Sound volume
 
-//--------------- FIN JS MEDIAPLAYER ------------------ //
 
-//permet d'animer le bouton play et d'actionner en même temps le player
+    $('#mute-toggle').on('click', function () {
+        var mute_toggle = $(this);
 
-const box = document.querySelector('.box');
-box.addEventListener('click', (e)=>{
-  e.target.classList.toggle('pause');
-  const playButtonArr = box.classList;
-  
-  for(let classPlayButton of playButtonArr)
-  {
-      if(classPlayButton === 'pause')
-      {
-          player.playVideo();
-      }
-      else
-      {
-          player.pauseVideo();
-      }
-  }
+        if (player.isMuted()) {
+            player.unMute();
+            mute_toggle.text('volume_up');
+        }
+        else {
+            player.mute();
+            mute_toggle.text('volume_off');
+        }
+    });
+
+    $('#volume-input').on('change', function () {
+        player.setVolume($(this).val());
+    });
+
+
+    // Other options
+
+
+    $('#speed').on('change', function () {
+        player.setPlaybackRate($(this).val());
+    });
+
+    $('#quality').on('change', function () {
+        player.setPlaybackQuality($(this).val());
+    });
+
+
+    // Playlist
+
+    $('#next').on('click', function () {
+        player.nextVideo()
+    });
+
+    $('#prev').on('click', function () {
+        player.previousVideo()
+    });
+
+
+    // Load video
+
+    $('.thumbnail').on('click', function () {
+
+        var url = $(this).attr('data-video-id');
+
+        player.cueVideoById(url);
+
+    });
+
+
+    // Helper Functions
+
+    function formatTime(time) {
+        time = Math.round(time);
+
+        var minutes = Math.floor(time / 60),
+            seconds = time - minutes * 60;
+
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+
+        return minutes + ":" + seconds;
+    }
+
+
+    $('pre code').each(function (i, block) {
+        hljs.highlightBlock(block);
+    });
+
+    //--------------- FIN JS MEDIAPLAYER ------------------ //
+
+    //permet d'animer le bouton play et d'actionner en même temps le player
+
+    const box = document.querySelector('.box');
+    box.addEventListener('click', (e) => {
+        e.target.classList.toggle('pause');
+        const playButtonArr = box.classList;
+
+        for (let classPlayButton of playButtonArr) {
+            if (classPlayButton === 'pause') {
+                player.playVideo();
+            }
+            else {
+                player.pauseVideo();
+            }
+        }
+    });
+
+
 }
