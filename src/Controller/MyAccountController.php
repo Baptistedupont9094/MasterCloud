1f4ccd22a13a46fc1a6381ded3bbd7633616e0b6
Léa Mahler <leamahler85@gmail.com>
2021-05-06 13:57:20 +0200

    pull request changes 2

--- src/Controller/MyAccountController.php
+++ src/Controller/MyAccountController.php
@@ -28,13 +28,13 @@ class MyAccountController extends AbstractController
             header('Location: /');
         } else {
             $playlistManager = new PlaylistManager();
-            $playlists = $playlistManager->selectAllPlaylistsbyUserID($_SESSION['user']['id']);
-            return $this->twig->render('MyAccount/index.html.twig',
-            [
-                'playlistsTwig' => $playlists,
-                'playlists' => (new PlaylistManager())->selectAll()
+
+            return $this->twig->render('MyAccount/index.html.twig', [
+                'playlistsTwig' => $playlistManager->selectAllPlaylistsbyUserID($_SESSION['user']['id']),
+                'playlists' => $playlistManager->selectAll()
             ]);
         }
+
         return $this->twig->render('MyAccount/index.html.twig');
     }
 }
